5752cce0113867dd3d2ab1e2c7b925d63ae423cc
Oscar Ossa <oskrlex17@gmail.com>
2024-09-27 10:31:22 -0500

    feat: add config, jobs and commands to main app

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
-	"os"
+	"log/slog"
 
+	"github.com/OzkrOssa/redplanet-telegram-bot/internal/adapter/commands"
+	"github.com/OzkrOssa/redplanet-telegram-bot/internal/adapter/config"
+	"github.com/OzkrOssa/redplanet-telegram-bot/internal/adapter/jobs"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func main() {
 
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_BOT_TOKEN"))
+	config, err := config.New()
+	if err != nil {
+		slog.Error("error to read config", "ERROR", err)
+	}
+
+	bot, err := tgbotapi.NewBotAPI(config.Telegram.BotToken)
 	if err != nil {
 		panic(err)
 	}
@@ -18,10 +26,20 @@ func main() {
 	updateConfig.Timeout = 30
 	updates := bot.GetUpdatesChan(updateConfig)
 
+	j := jobs.NewMonitorJobs(bot, *config.RouterOsApi, *config.Telegram)
+	err = j.Run()
+	if err != nil {
+		slog.Error("error to running jobs", "ERROR", err)
+	}
+
 	for update := range updates {
 
 		if update.Message != nil {
-			continue
+			adapter := commands.NewCommandService(bot, &update, *config.RouterOsApi)
+			err := adapter.ProcessCommand()
+			if err != nil {
+				slog.Error("command error", "ERROR", err)
+			}
 		}
 	}
 
