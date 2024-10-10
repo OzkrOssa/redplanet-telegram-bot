4edb46b1e39cb749a48567de8fbdc4d1ab3894e2
Oscar Ossa <oskrlex17@gmail.com>
2024-10-10 15:26:44 -0500

    feat: rename struct and constructor and implement keyboard for /core command\

--- internal/adapter/commands/commands.go
+++ internal/adapter/commands/commands.go
@@ -1,45 +1,39 @@
 package commands
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"strconv"
-
 	"github.com/OzkrOssa/redplanet-telegram-bot/internal/adapter/config"
-	"github.com/OzkrOssa/redplanet-telegram-bot/internal/core/domain"
-	"github.com/OzkrOssa/redplanet-telegram-bot/internal/core/service"
-	"github.com/OzkrOssa/redplanet-telegram-bot/internal/core/utils"
+	"github.com/OzkrOssa/redplanet-telegram-bot/internal/adapter/keyboard"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-type CommandService struct {
+type CommandHandler struct {
 	bot    *tgbotapi.BotAPI
 	update *tgbotapi.Update
 	config config.RouterOsApi
 }
 
-func NewCommandService(bot *tgbotapi.BotAPI, update *tgbotapi.Update, config config.RouterOsApi) *CommandService {
-	return &CommandService{
+func NewCommandHandler(bot *tgbotapi.BotAPI, update *tgbotapi.Update, config config.RouterOsApi) *CommandHandler {
+	return &CommandHandler{
 		bot,
 		update,
 		config,
 	}
 }
 
-func (cs *CommandService) ProcessCommand() error {
+func (cs *CommandHandler) HandlerCommands() error {
 	command := cs.update.Message.Command()
 
 	switch command {
 	case "start":
 		return cs.start()
 	case "core":
-		return cs.getCoreTraffic()
+		return cs.SendKeyboardCoreRiosucio()
+
 	}
 	return nil
 }
 
-func (cs *CommandService) start() error {
+func (cs *CommandHandler) start() error {
 
 	startText := "Welcome to Red Planet Bot"
 	message := tgbotapi.NewMessage(cs.update.Message.Chat.ID, startText)
@@ -48,36 +42,11 @@ func (cs *CommandService) start() error {
 	return err
 }
 
-func (cs *CommandService) getCoreTraffic() error {
+func (cs *CommandHandler) SendKeyboardCoreRiosucio() error {
+	msg := tgbotapi.NewMessage(cs.update.Message.Chat.ID, cs.update.Message.Text)
+	msg.ReplyMarkup = keyboard.CoreRiosucioKeyboard
 
-	service, err := service.NewMikrotikService(os.Getenv("CORE_ADDRESS"), cs.config)
-	if err != nil {
-		return err
-	}
+	_, err := cs.bot.Send(msg)
+	return err
 
-	traffic, err := service.GetTraffic(string(domain.SFP1))
-	if err != nil {
-		return err
-	}
-
-	tx, _ := strconv.Atoi(traffic.Tx)
-	rx, _ := strconv.Atoi(traffic.Rx)
-
-	resources, err := service.GetResources()
-	if err != nil {
-		return err
-	}
-
-	log.Println(traffic.Source, traffic, resources)
-
-	textMessage := fmt.Sprintf("<b>%s</b>\n<b><i>Iface:</i></b> %s\n<b><i>Cpu:</i></b> %s\n<b><i>Uptime:</i></b> %s\n<b><i>Rx:</i></b> %s\n<b><i>Tx:</i></b> %s", *traffic.Source, domain.SFP1, resources.Cpu, resources.Uptime, utils.FormatSize(int64(rx)), utils.FormatSize(int64(tx)))
-
-	message := tgbotapi.NewMessage(cs.update.Message.Chat.ID, textMessage)
-	message.ParseMode = "Html"
-	_, err = cs.bot.Send(message)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
