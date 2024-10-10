20f1aa1b828f2165744c093d622b65ce987dee8a
Oscar Ossa <oskrlex17@gmail.com>
2024-10-10 15:24:39 -0500

    feat: add inlinekeyboard with button options for /core command

---
+++ internal/adapter/keyboard/core_riosuco_keyboard.go
@@ -0,0 +1,17 @@
+package keyboard
+
+import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+
+var CoreRiosucioKeyboard = tgbotapi.NewInlineKeyboardMarkup(
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("Azteca", "azteca"),
+		tgbotapi.NewInlineKeyboardButtonData("Masivos", "masivos"),
+	),
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("Supia", "supia"),
+		tgbotapi.NewInlineKeyboardButtonData("San Lorenzo", "sl"),
+	),
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("Comerciales", "comerciales"),
+	),
+)
